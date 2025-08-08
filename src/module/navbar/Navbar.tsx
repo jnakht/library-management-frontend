92d4fcbec477d9f4abc3c07fcf154ae33df46d4f
jnakht <jisan.cse.cu@gmail.com>
2025-08-08 09:59:12 +0600

    navbar initialized, react icons installed

--- src/module/navbar/Navbar.tsx
+++ src/module/navbar/Navbar.tsx
@@ -1,10 +1,22 @@
-
-
+import { NavbarMenu } from "./data";
+import { IoLibrary } from "react-icons/io5";
+import { CiSearch } from "react-icons/ci";
+import { PiShoppingCart } from "react-icons/pi";
+import { MdMenu } from "react-icons/md";
 
 export default function Navbar() {
   return (
-    <div>
-      <h3>This is Navbar component</h3>
-    </div>
+    <>
+      <nav>
+        <div>
+            {/* logo */}
+            {/* desktop links */}
+            {/* icons */}
+            {/* mobile hamburger */}
+        </div>
+      </nav>
+
+      {/* mobile links */}
+    </>
   );
 }
---
+++ src/module/navbar/data.ts
@@ -0,0 +1,23 @@
+
+export const NavbarMenu = [
+    {
+        id: 1,
+        title: 'Home',
+        link: '/'
+    },
+    {
+        id: 2,
+        title: 'Books',
+        link: '#'
+    },
+    {
+        id: 3,
+        title: 'AddBook',
+        link: '#'
+    },
+    {
+        id: 4,
+        title: 'About',
+        link: '#'
+    },
+]

047a40073fb8680de687c213a135a8f9e5761497
jnakht <jisan.cse.cu@gmail.com>
2025-08-11 09:46:18 +0600

    borrow summary table added

--- src/pages/BorrowSummary.tsx
+++ src/pages/BorrowSummary.tsx
@@ -5,8 +5,30 @@ export default function BorrowSummary() {
     const { data, isLoading } = useBorrowSummaryQuery(undefined);
     console.log(data);
   return (
-    <div>
-      <h3>This is BorrowSummary component</h3>
+    <div className="max-w-[80%] mx-auto">
+      <div className="overflow-x-auto">
+        <table className="w-full border border-gray-200 text-sm text-left text-gray-700">
+            <thead className="bg-[#7626FD] text-white text-xs uppercase ">
+                <tr className="">
+                    <th className="px-4 py-3 border-b">Book Title</th>
+                    <th className="px-4 py-3 border-b">ISBN</th>
+                    <th className="px-4 py-3 border-b">Total Quantity Borrowed</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    data?.data?.map( item => 
+                        <tr key={item?.book?.isbn} 
+                        className="border-b hover:bg-gray-50">
+                            <td className="px-4 py-3">{item?.book?.title || 'unknown/book deleted'}</td>
+                            <td className="px-4 py-3">{item?.book?.isbn || 'N/A'}</td>
+                            <td className="px-4 py-3">{item?.totalQuantity}</td>
+                        </tr>
+                    )
+                }
+            </tbody>
+        </table>
+      </div>
     </div>
   );
 }
